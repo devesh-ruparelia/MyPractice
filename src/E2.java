f768e64e10283f011da8362d9573d9794fdb7776
deveshr <devesh.r@ezdi.us>
2019-07-22 13:10:43 +0530

    Initial Commit

--- src/E2.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class E2 {
-
-    public static void main(String[] args) {
-        Map<Integer,String> map = new HashMap<>();
-    }
-}
---
+++ src/main/java/P4.java
@@ -0,0 +1,13 @@
+public class P4 {
+    public static void main(String[] args) {
+        char ch1='A';
+        char ch2='a';
+        char ch3='B';
+        char ch4='b';
+
+        System.out.println(ch1-ch2);
+        System.out.println(ch3-ch4);
+        Character ch ='a';
+    }
+
+}
